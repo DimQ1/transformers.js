de83046cde321f2d439edd4a6308836859655e5b
Дмитрий <dimq1@ya.ru>
2023-04-01 14:58:13 +0300

    tem changes

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,7 @@
 const { pipeline, env } = require('..');
+const fs = require('fs');
+var AudioContext = require('web-audio-api').AudioContext,
+  context = new AudioContext();
 
 // Only use local models
 env.remoteModels = false;
@@ -6,10 +9,20 @@ env.remoteModels = false;
 
 async function speech2text_generation() {
   // TODO add test case
-  let audio =
-    'd:\\Users\\Dimq1\\source\\OpenAI\\transformers.js\\assets\\audio\\jfk.wav ';
-  let transcriber = await pipeline('automatic-speech-recognition');
-  let output = await transcriber(audio);
+  let filePath = `d:/Users/Dimq1/source/OpenAI/transformers.js/assets/audio/zapis-razgovora-vika-elina-zvonit-mne_.wav`;
+
+  // fs.readFile(filePath, (err, data) => {
+  //   if (err) throw err;
+  //   context.decodeAudioData(data.buffer, (err) => {
+  //     console.error(err);
+  //   });
+  // });
+
+  let transcriber = await pipeline(
+    'automatic-speech-recognition',
+    'whisper-medium'
+  );
+  let output = await transcriber(filePath);
   console.log(output);
 
   return [true, 0];
